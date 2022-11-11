d434271c75409bb2352815216dd70ecf36354979
gitongasam <gitongas684@gmail.com>
2022-11-11 08:10:21 +0300

    commit

--- src/main/java/com/devsam/Banksystem/Entity/Customer.java
+++ src/main/java/com/devsam/Banksystem/Entity/Customer.java
@@ -1,4 +1,18 @@
 package com.devsam.Banksystem.Entity;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Entity;
+
+@Entity
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
 public class Customer {
+    private String firstName;
+    private  String SecondName;
+    private String lastname;
+    private String password;
 }
