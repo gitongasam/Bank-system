9e75059bf25563cc9f19c0bc81bcf659dceab72d
gitongasam <gitongas684@gmail.com>
2022-11-11 08:24:37 +0300

    commit

--- src/main/java/com/devsam/Banksystem/Entity/CustomerModel.java
+++ src/main/java/com/devsam/Banksystem/Entity/CustomerModel.java
@@ -4,18 +4,10 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
-
-@Entity
 @AllArgsConstructor
 @NoArgsConstructor
 @Data
-public class Customer {
-    @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
+public class CustomerModel {
     private String Id;
     private String firstName;
     private  String SecondName;
